9459b56f3c7664f9e1807cd56b6dd7d5e76dfefa
JBOGACZ <Rower123>
2017-05-30 12:32:29 +0200

    Rules logic

--- src/main/java/com/jakubbogacz/RuleListContext.java
+++ src/main/java/com/jakubbogacz/RuleListContext.java
@@ -17,6 +17,6 @@ public class RuleListContext {
         RULE_LIST.add(new Rule(
                 "Gigabyte Radeon RX 570 GAMING 4GB GDDR5",
                 "http://www.x-kom.pl/p/359723-karta-graficzna-pci-e-gigabyte-radeon-rx-570-gaming-4gb-gddr5.html",
-                ".product-infobox p button:not([class*='temporary-unavailable2'])"));
+                ".product-infobox p button:not([class*='temporary-unavailable'])"));
     }
 }
