1f94ef44fc8b97d1b1edfb0dbd98b3aa35ddc866
jbogacz <Rower123>
2017-05-30 08:17:13 +0200

    Scan selector

--- src/main/java/com/jakubbogacz/ItemAvailabilityCheckerApplication.java
+++ src/main/java/com/jakubbogacz/ItemAvailabilityCheckerApplication.java
@@ -2,8 +2,10 @@ package com.jakubbogacz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ItemAvailabilityCheckerApplication {
 
 	public static void main(String[] args) {
