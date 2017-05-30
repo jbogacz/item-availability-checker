9aca8ae6c45b7746399662e7e634803db8228e81
jbogacz <Rower123>
2017-05-30 07:37:26 +0200

    initial commit

---
+++ src/main/java/com/jakubbogacz/ItemAvailabilityCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.jakubbogacz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemAvailabilityCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemAvailabilityCheckerApplication.class, args);
+	}
+}
